6391e16f48bddfee6c3690b788217968b5a4f926
yoyuse <yoyuse@gmail.com>
2025-04-01 21:00:00 +0900

    newjis101

---
+++ extra/newjis101.el
@@ -0,0 +1,279 @@
+;;; newjis101.el --- new JIS kana input on 101 keyboard  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2023, 2025  YUSE Yosihiro
+
+;; Author: YUSE Yosihiro <yoyuse@gmail.com>
+;; Keywords: multilingual, input method, Japanese
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This file is based on japanese.el
+
+;;; Setup:
+
+;; (require 'newjis101 nil t)
+;;
+;; (setq default-input-method "newjis101")
+;;
+;; (define-key kkc-keymap (kbd "H") nil)
+;; (define-key kkc-keymap (kbd "K") nil)
+;; (define-key kkc-keymap (kbd "O") nil)
+;; (define-key kkc-keymap (kbd "I") nil)
+;; (define-key kkc-keymap (kbd "l") nil)
+;; (define-key kkc-keymap (kbd "L") nil)
+;;
+;; (define-key kkc-keymap (kbd "C-u") 'kkc-hiragana)
+;; (define-key kkc-keymap (kbd "C-k") 'kkc-katakana)
+;; (define-key kkc-keymap (kbd "C-l") 'kkc-longer-phrase)
+;; (define-key kkc-keymap (kbd "C-y") 'kkc-shorter-conversion)
+;; (define-key kkc-keymap (kbd "C-v") 'kkc-show-conversion-list-or-next-group)
+;; (define-key kkc-keymap (kbd "M-v") 'kkc-show-conversion-list-or-prev-group)
+
+;;; Code:
+
+(require 'quail)
+(require 'kkc)
+
+;; XXX
+(defun quail-newjis101-update-translation (control-flag)
+  ;; (message "<%S><%S><%S>" control-flag quail-current-key quail-current-str)
+  (cond ((null control-flag)
+         ;; (setq quail-current-str (or quail-current-str quail-current-key)))
+         (setq quail-current-str
+               (if (/= (aref quail-current-key 0) ?\;)
+                   (or quail-current-str quail-current-key)
+                 "")))
+        ;;
+        ((integerp control-flag)
+         (setq unread-command-events
+               (append (substring quail-current-key control-flag)
+                       unread-command-events)))
+        ((eq t control-flag)
+         (save-excursion
+           ;; (setq quail-translating nil) ; XXX: ???
+           (let* ((beg (overlay-start quail-conv-overlay))
+                  (end (overlay-end quail-conv-overlay))
+                  (point (point))
+                  char)
+             (cond ((and (number-or-marker-p beg) (number-or-marker-p end)
+                         (< beg point) (string= "l" quail-current-key))
+                    (goto-char (1- point))
+                    (setq char (char-after))
+                    (cond ((looking-at-p
+                            "[うかきくけこさしすせそたちつてとはひふへほ]")
+                           (setq quail-current-str "")
+                           (insert (if (= ?う char) ?ヴ (1+ char))) ; 濁音化
+                           (delete-char 1)
+                           ;;
+                           (setq quail-conversion-str
+                                 (buffer-substring beg end))
+                           ;;
+                           )))
+                   ((and (number-or-marker-p beg) (number-or-marker-p end)
+                         (< beg point) (string= "W" quail-current-key))
+                    (goto-char (1- point))
+                    (setq char (char-after))
+                    (cond ((looking-at-p "[はひふへほ]")
+                           (setq quail-current-str "")
+                           (insert (+ 2 char)) ; 半濁音化
+                           (delete-char 1)
+                           ;;
+                           (setq quail-conversion-str
+                                 (buffer-substring beg end))
+                           ;;
+                           )
+                          ((looking-at-p "[わいえ]")
+                           (setq quail-current-str "")
+                           (insert (cond ((= ?わ char) ?ゎ)
+                                         ((= ?い char) ?ゐ)
+                                         ((= ?え char) ?ゑ)))
+                           (delete-char 1)
+                           ;;
+                           (setq quail-conversion-str
+                                 (buffer-substring beg end))
+                           ;;
+                           )
+                          ((looking-at-p "[かけ]")
+                           (setq quail-current-str "")
+                           (insert (if (= ?か char) ?ヵ ?ヶ))
+                           (delete-char 1)
+                           ;;
+                           (setq quail-conversion-str
+                                 (buffer-substring beg end))
+                           ;;
+                           )))
+                   (t nil))
+             )))
+        (t nil))
+  control-flag)
+
+(defun quail-newjis101-toggle-kana ()
+  (interactive)
+  (setq quail-translating nil)
+  (let ((start (overlay-start quail-conv-overlay))
+	(end (overlay-end quail-conv-overlay)))
+    (save-excursion
+      (goto-char start)
+      (if ;; (re-search-forward "\\cH" end t)
+          (looking-at "\\cH")
+          ;;
+	  (japanese-katakana-region start end)
+	(japanese-hiragana-region start end)))
+    (setq quail-conversion-str
+	  (buffer-substring (overlay-start quail-conv-overlay)
+			    (overlay-end quail-conv-overlay)))
+    ;; XXX
+    (setq quail-current-key nil)
+    (setq quail-current-str nil)
+    ;;
+    ))
+
+(defun quail-newjis101-kanji-kkc ()
+  (interactive)
+  ;; (when (= (char-before (overlay-end quail-conv-overlay)) ?n)
+  ;;   ;; The last char is `n'.  We had better convert it to `ん'
+  ;;   ;; before kana-kanji conversion.
+  ;;   (goto-char (1- (overlay-end quail-conv-overlay)))
+  ;;   (insert ?ん)
+  ;;   (delete-char 1))
+  (let* ((from (copy-marker (overlay-start quail-conv-overlay)))
+	 (len (- (overlay-end quail-conv-overlay) from)))
+    (quail-delete-overlays)
+    (setq quail-current-str nil)
+    (unwind-protect
+	(let ((result (kkc-region from (+ from len))))
+	  (move-overlay quail-conv-overlay from (point))
+	  (setq quail-conversion-str (buffer-substring from (point)))
+	  (if (= (+ from result) (point))
+	      (setq quail-converting nil))
+	  (setq quail-translating nil))
+      (set-marker from nil))))
+
+(defun quail-newjis101-no-conversion ()
+  "Terminate the translation. Do no conversion."
+  (interactive)
+  (setq quail-translating nil)
+  (setq quail-converting nil))
+
+(defvar quail-newjis101-transliteration-rules
+  '(( "b" "あ") ( "k" "い") ( "j" "う") ( "U" "え") ( "J" "お")
+    ( "s" "か") ( ";" "き") ( "h" "く") ( "w" "け") ( "x" "こ")
+    ( "v" "さ") ( "d" "し") ( "z" "す") ( "e" "せ") ( "q" "そ")
+    ( "g" "た") ( "[" "ち") ( "y" "つ") ( "r" "て") ( "f" "と")
+    ( "'" "な") ( "c" "に") ( "P" "ぬ") ( "V" "ね") ( "i" "の")
+    ( "a" "は") ( "Y" "ひ") ( "R" "ふ") ( "S" "へ") ( "E" "ほ")
+    ( "H" "ま") ( "I" "み") ( "N" "む") ( "T" "め") ( "K" "も")
+    ( "O" "や")             ( ":" "ゆ")             ( "G" "よ")
+    ( "D" "ら") ( "p" "り") ( "m" "る") ( "/" "れ") ( "M" "ろ")
+    ( "L" "わ") ( "o" "を") ( "u" "ん")
+    ( "Q" "ぁ") ( "A" "ぃ") ( "Z" "ぅ") ( "X" "ぇ") ( "C" "ぉ")
+    ( "n" "っ") ( "B" "ゃ") ( "F" "ゅ") ( "t" "ょ")
+    ( ">" "ー") ( "{" "「") ( "}" "」") ( "," "、") ( "." "。")
+    ( "<" "・") ( "l" "゛") ( "W" "゜")
+    ;; ("sl" "が") (";l" "ぎ") ("hl" "ぐ") ("wl" "げ") ("xl" "ご")
+    ;; ("vl" "ざ") ("dl" "じ") ("zl" "ず") ("el" "ぜ") ("ql" "ぞ")
+    ;; ("gl" "だ") ("[l" "ぢ") ("yl" "づ") ("rl" "で") ("fl" "ど")
+    ;; ("al" "ば") ("Yl" "び") ("Rl" "ぶ") ("Sl" "べ") ("El" "ぼ")
+    ;; ("aW" "ぱ") ("YW" "ぴ") ("RW" "ぷ") ("SW" "ぺ") ("EW" "ぽ")
+    ;; ("LW" "ゎ") ("kW" "ゐ")             ("UW" "ゑ")
+    ;; ("jl" "ヴ") ("sW" "ヵ") ("wW" "ヶ")
+
+    ("$1" "○") ("$!" "●")
+    ("$2" "▽") ("$@" "▼")
+    ("$3" "△") ("$#" "▲")
+    ("$4" "□") ("$$" "■")
+    ("$5" "◇") ("$%" "◆")
+    ("$6" "☆") ("$^" "★")
+    ("$7" "◎") ("$&" "£")
+    ("$8" "¢") ("$*" "×")
+    ("$9" "♂") ("$(" "【")
+    ("$0" "♀") ("$)" "】")
+    ("$-" "〜") ("$_" "∴")
+    ("$=" "≠") ("$+" "±")
+    ("$\\" "＼") ("$|" "‖")
+    ("$`" "´") ("$~" "¨")
+
+    ("$q" "《") ("$Q" "〈")
+    ("$w" "》") ("$W" "〉")
+    ("$r" "々") ("$R" "仝")
+    ("$t" "〆") ("$T" "§")
+    ("$p" "〒") ("$P" "↑")
+    ("$[" "『") ("${" "〔")
+    ("$]" "』") ("$}" "〕")
+
+    ("$s" "ヽ") ("$S" "ヾ")
+    ("$d" "ゝ") ("$D" "ゞ")
+    ("$f" "〃") ("$F" "→")
+    ("$g" "‐") ("$G" "—")
+    ("$h" "←")
+    ("$j" "↓")
+    ("$k" "↑")
+    ("$l" "→")
+    ("$;" "゛") ("$:" "゜")
+    ("$'" "‘") ("$\"" "“")
+
+    ;; "-" が "ー" になってしまって具合が悪い
+    ;; ("$x" [":-"]) ("$X" [":-)"])
+    ;; XXX: 全角引用符の救済策
+    ("$x" "’") ("$X"  "”")
+
+    ("$c" "〇") ("$C" "℃")
+    ("$v" "※") ("$V" "÷")
+    ("$b" "°") ("$B" "←")
+    ("$n" "′") ("$N" "↓")
+    ("$m" "″") ("$M" "〓")
+    ("$," "‥") ("$<" "≦")
+    ("$." "…") ("$>" "≧")
+    ("$/" "・") ("$?" "∞")
+    ))
+
+(quail-define-package
+ "newjis101"
+ "Japanese"
+ "新" ; "新JISかな"
+ nil
+ "new JIS kana input on 101 keyboard.
+
+そけせてょ つんのをりち    ぁ゜ほふめ ひえみやぬ「」
+ はかしとた くうい゛きな    ぃへらゅよ まおもわゆ
+  すこにさあ っる、。れ      ぅぇぉねゃ むろ・ー
+"
+ nil
+ t
+ t
+ nil
+ nil
+ nil
+ nil
+ nil
+ #'quail-newjis101-update-translation
+ '(;; ("K" . quail-newjis101-toggle-kana)
+   ;; ("\C-m" . quail-no-conversion)
+   ;; ([return] . quail-no-conversion)
+   ("\C-k" . quail-newjis101-toggle-kana)
+   (" " . quail-newjis101-kanji-kkc)
+   ("\C-m" . quail-newjis101-no-conversion)
+   ([return] . quail-newjis101-no-conversion))
+ t
+ )
+
+(dolist (elt quail-newjis101-transliteration-rules)
+  (quail-defrule (car elt) (nth 1 elt)))
+
+;;; provide
+
+(provide 'newjis101)
+;;; newjis101.el ends here
---
+++ extra/newjis101.md
@@ -0,0 +1,40 @@
+# newjis101
+
+## これは何?
+
+leim/quail/japanese.el (ローマ字入力) の代わりに 新 JIS かな配列 で入力を行うためのパッケージです。
+
+実際の配列を図に示します
+(例は Happy Hacking Keyboard 英語配列)。
+
+![newjis101](img/newjis101.svg)
+
+## 使用法
+
+newjis101.el を `load-path` の通ったところに置いて init.el に次のように書きます。
+
+``` emacs-lisp
+(require 'newjis101)
+;; 好みに合わせて
+;; (setq default-input-method "newjis101")
+;;
+;; (define-key kkc-keymap (kbd "H") nil)
+;; (define-key kkc-keymap (kbd "K") nil)
+;; (define-key kkc-keymap (kbd "O") nil)
+;; (define-key kkc-keymap (kbd "I") nil)
+;; (define-key kkc-keymap (kbd "l") nil)
+;; (define-key kkc-keymap (kbd "L") nil)
+;;
+;; (define-key kkc-keymap (kbd "C-u") 'kkc-hiragana)
+;; (define-key kkc-keymap (kbd "C-k") 'kkc-katakana)
+;; (define-key kkc-keymap (kbd "C-l") 'kkc-longer-phrase)
+;; (define-key kkc-keymap (kbd "C-y") 'kkc-shorter-conversion)
+;; (define-key kkc-keymap (kbd "C-v") 'kkc-show-conversion-list-or-next-group)
+;; (define-key kkc-keymap (kbd "M-v") 'kkc-show-conversion-list-or-prev-group)
+```
+
+`(setq default-input-method "newjis101")` として `C-\` とするか、
+`C-u C-\` `newjis101` `RET` とすると、
+日本語入力モードに切り替わり、新 JIS かな配列で入力できます。
+
+再度 `C-\` とすると、英字モードに戻ります。
